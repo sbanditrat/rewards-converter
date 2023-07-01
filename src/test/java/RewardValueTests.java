4dcaf9894dd518241d8881315db720d9c4c89eb5
Sherry Banditrat <sherrybanditrat@gmail.com>
2023-07-01 16:27:46 -0500

    Merge reward-value to flow

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,20 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int convertMilesValue = rewardValue.convertToMiles(cashValue);
+        int expectedMilesValue =28571;
+        assertEquals(expectedMilesValue, convertMilesValue);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue= new RewardValue(milesValue);
+
+        double convertedCashValue = rewardValue.convertToCash(milesValue);
+        double expectedCashValue = 35.0;
+        assertEquals(expectedCashValue, convertedCashValue);
     }
 }
